7284b425a547dff6f60de68b06a84f68949babf0
vipolar <miskuzius@gmail.com>
2023-11-10 12:25:02 +0000

    scripts updated to hell!

--- docker-build.sh
+++ docker-build.sh
@@ -1,16 +1,80 @@
-# Create local files
-mkdir -p pgadmin/var/lib/pgadmin/sessions
-echo "{}" > pgadmin/servers.json
-mkdir postgresdb
+#!/bin/bash
 
-# Modify permissions
-sudo chown -R unulearner:unulearner ./pgadmin
-sudo chmod -R o+w ./pgadmin
-sudo chown -R unulearner:unulearner ./postgresdb
+script_file="./$(basename "${BASH_SOURCE[0]}")"
+unulearner_frontend_path="./unulearner-frontend"
+
+if [ "$EUID" -eq 0 ]; then
+    echo "ABORTING: script was invoked by a super user!"
+    echo "HINT: no sudoers allowed!"
+    exit
+fi
+
+if [ "$0" != "$script_file" ]; then
+    echo "ABORTING: script invocation error occurred!"
+    echo "HINT: invoke the script as a local executable: ./example.sh"
+    exit
+fi
+
+if [ -f .env ]; then
+    # Load variables from .env file excluding comments
+    while IFS= read -r line; do
+        if [[ "$line" =~ ^[^#]*= ]]; then
+            export "$line"
+        fi
+    done < .env
+
+    # Export variables with self-references using envsubst
+    envsubst < .env > .env.tmp
+    source .env.tmp
+    rm .env.tmp
+else
+    echo "ABORTING: .env file not found!"
+    exit
+fi
+
+if [ -f ./unulearner-resources/nginx/conf.template/default.conf.template ]; then
+    mkdir -p nginx/conf.d/
+    sudo chmod -R o+w ./nginx
+
+    envsubst < ./unulearner-resources/nginx/conf.template/default.conf.template > ./nginx/conf.d/default.conf
+    echo "Success: nginx configuration ready!"
+else
+    echo "ABORTING: nginx configuration failed! (template file not found)"
+    exit
+fi
+
+if [ -f ./unulearner-resources/pgadmin/servers.json.template ]; then
+    mkdir -p pgadmin/var/lib/pgadmin/sessions
+    sudo chmod -R o+w ./pgadmin
+
+    envsubst < ./unulearner-resources/pgadmin/servers.json.template > ./pgadmin/servers.json
+    echo "Success: pgadmin servers configuration ready!"
+else
+    echo "ABORTING: pgadmin configuration failed! (template file not found)"
+    exit
+fi
+
+# Create rest of the local directories
+mkdir -p postgresdb/data
 sudo chmod -R o+w ./postgresdb
 
-# Pull unulearner packages
-git clone https://github.com/vipolar/unulearner-frontend.git
+# Pull unulearner repositories
+if [ -d "$unulearner_frontend_path" ]; then
+    echo "Folder '$unulearner_frontend_path' exists, meaning the repository has already been cloned locally!"
+    echo "Would you like remove the existing local repository and clone it again? [y/N]: "
+    
+    read confirmation
+    confirmation_lower=$(echo "$confirmation" | tr '[:upper:]' '[:lower:]')
+
+    if [ "$confirmation_lower" = "y" ]; then
+        rm -rf $unulearner_frontend_path
+        git clone https://github.com/vipolar/unulearner-frontend.git
+    else
+        echo "Cloning repository skipped"
+    fi
+else
+    git clone https://github.com/vipolar/unulearner-frontend.git
+fi
 
 # Launch
 sudo docker compose up --build
--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -1,7 +1,57 @@
-sudo docker compose down --volumes
-sudo docker system prune #-a -f #NUCLEAR OPTION!
-sudo docker volume rm $(sudo docker volume ls -q --filter dangling=true)
+#!/bin/bash
 
-# Delete local files
-sudo rm -rf pgadmin
-sudo rm -rf postgresdb
+script_file="./$(basename "${BASH_SOURCE[0]}")"
+
+if [ "$EUID" -eq 0 ]; then
+    echo "ABORTING: script was invoked by a super user!"
+    echo "HINT: no sudoers allowed!"
+    exit
+fi
+
+if [ "$0" != "$script_file" ]; then
+    echo "ABORTING: script invocation error occurred!"
+    echo "HINT: invoke the script as a local executable: ./example.sh"
+    exit
+fi
+
+sudo docker compose down
+
+echo "Enter 'NUCLEAR' if you want to destroy everything associated with this docker compose:"
+read confirmation
+
+if [ "$confirmation" = "NUCLEAR" ]; then
+    sudo docker system prune -a -f #NUCLEAR OPTION!
+
+    sudo rm -rf nginx
+    sudo rm -rf pgadmin
+    sudo rm -rf postgresdb
+else
+    echo "Would you like to remove dangling volumes? [y/N]: "
+
+    read confirmation
+    confirmation_lower=$(echo "$confirmation" | tr '[:upper:]' '[:lower:]')
+
+    if [ "$confirmation_lower" = "y" ]; then
+        sudo docker volume rm $(sudo docker volume ls -q --filter dangling=true)
+    fi
+
+    echo "Would you like to remove all remaining volumes? [y/N]: "
+
+    read confirmation
+    confirmation_lower=$(echo "$confirmation" | tr '[:upper:]' '[:lower:]')
+
+    if [ "$confirmation_lower" = "y" ]; then
+        sudo docker volume rm $(docker volume ls -q)
+    fi
+
+    echo "Would you like to remove the local directories associated with the docker volumes? [y/N]: "
+
+    read confirmation
+    confirmation_lower=$(echo "$confirmation" | tr '[:upper:]' '[:lower:]')
+
+    if [ "$confirmation_lower" = "y" ]; then
+        sudo rm -rf nginx
+        sudo rm -rf pgadmin
+        sudo rm -rf postgresdb
+    fi
+fi
